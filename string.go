02225ef5b49b11bf6f70df14c7f13bab539f25c7
DhandyPF <dhandy.pf@gmail.com>
2026-05-10 13:19:09 +0700

    I learned about string function

--- string.go
+++ string.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("Dhandy")
 	fmt.Println("Dhandy Putra")
 	fmt.Println("Dhandy Putra Fahruddin")
+	
+	fmt.Println(len("Dhandy"))
+	fmt.Println("Dhandy Putra"[0])
+	fmt.Println("Dhandy Putra Fahruddin"[5])
 }
