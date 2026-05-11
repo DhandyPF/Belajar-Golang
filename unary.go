d896ce12d6d07df16420230865f4205f7723d2ba
DhandyPF <dhandy.pf@gmail.com>
2026-05-11 20:34:08 +0700

    Add convertion.go, and I learned about Unary operator in Golang

---
+++ unary.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var j = 1
+
+	// Increment
+	j++ // j = j + 1
+	fmt.Println(j)
+	j++ // j = j + 1
+	fmt.Println(j)
+
+	// Decrement
+	j-- // j = j - 1
+	fmt.Println(j)
+	j-- // j = j - 1
+	fmt.Println(j)
+}
