0b5d914f83c4a9104d1c12f70b6ffa791172f754
DhandyPF <dhandy.pf@gmail.com>
2026-05-11 21:00:47 +0700

    Add convertion.go, and I learned about Boolean operators in Golang

---
+++ boolean_operator.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+func main() {
+	// Boolean Operator
+	var nilaiAkhir = 90
+	var absensi = 80
+
+	var lulusNilaiAkhir bool = nilaiAkhir > 80
+	var lulusAbsensi bool = absensi > 80
+
+	var lulus bool = lulusNilaiAkhir && lulusAbsensi
+	var lulus2 bool = lulusNilaiAkhir || lulusAbsensi
+	fmt.Println(lulus)
+	fmt.Println(lulus2)
+}
