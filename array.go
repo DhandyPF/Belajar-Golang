e5f0d14e6bdee7643495b33a0d83f2eef6eea761
DhandyPF <dhandy.pf@gmail.com>
2026-05-11 21:16:52 +0700

    Add convertion.go, and I learned about Array in Golang

---
+++ array.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name [3]string
+
+	name[0] = "Dhandy"
+	name[1] = "Putra"
+	name[2] = "Fahruddin"
+	fmt.Println(name[0])
+	fmt.Println(name[1])
+	fmt.Println(name[2])
+
+	var values = [3]int{10, 3, 2007}
+
+	fmt.Println(values)
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+
+	var values2 = [...]int{
+		1,
+		2,
+		3,
+		4,
+		5,
+	}
+
+	fmt.Println(values2)
+
+	fmt.Println(len(values))
+	fmt.Println(len(values2))
+}
