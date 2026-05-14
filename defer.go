4f3d391ccf409be684653965ed6c1b78abefc130
DhandyPF <dhandy.pf@gmail.com>
2026-05-14 23:15:21 +0700

    Add defer.go, and I learned about Defer in Golang

---
+++ defer.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func logging() {
+	fmt.Println("Selesai memanggil function")
+}
+
+func runApplication() {
+	defer logging()
+
+	fmt.Println("Run Application")
+	// // Error
+	// logging()
+}
+
+func main() {
+	runApplication()
+}
