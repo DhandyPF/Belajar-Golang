eca336ad137237a0df57c01cc01a18ba5d734c94
DhandyPF <dhandy.pf@gmail.com>
2026-05-15 00:02:02 +0700

    Add struct.go, and I learned about Struct in Golang

--- struct.go
+++ struct.go
@@ -2,25 +2,32 @@ package main
 
 import "fmt"
 
-type customer struct {
+type Customer struct {
 	Name, Address string
 	Age int
 }
 
+func(customer Customer) sayHello(name string) {
+	fmt.Println("Hello", name, "My name is", customer.Name)
+}
+
 func main() {
-	var dimas customer
+	var dimas Customer
 	dimas.Name = "Dimas,"
 	dimas.Address = "Depok,"
 	dimas.Age = 100
 	fmt.Println(dimas)
 
-	willy := customer {
+	willy := Customer {
 		Name: "Willy,",
 		Address: "Sokaraja,",
 		Age: 11,
 	}
 	fmt.Println(willy)
 
-	kautsar := customer{"Kautsar,", "PWT,", 10}
+	kautsar := Customer{"Kautsar,", "PWT,", 10}
 	fmt.Println(kautsar)
+
+	bintang := Customer{"Bintang,", "Susukan,", 22}
+	bintang.sayHello("Bintang")
 }
