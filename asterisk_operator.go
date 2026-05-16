a5b26a062d0a1de4cdc3349ea2977c970ccb2b01
DhandyPF <dhandy.pf@gmail.com>
2026-05-16 21:20:54 +0700

    Add asterisk_operator.go, and I learned about Asterisk Operator in Golang

---
+++ asterisk_operator.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	// address1 := Address{"Purwokerto", "Jawa Tengah", "Indonesia"}
+	// address2 := address1
+
+	// address2.City = "Purbalingga"
+
+	// fmt.Println(address1)
+	// fmt.Println(address2)
+
+	var address1 Address = Address{"Purwokerto", "Jawa Tengah", "Indonesia"}
+	var address2 *Address = &address1
+	address2.City = "Purbalingga"
+	fmt.Println(address1)
+	fmt.Println(address2)
+
+	// address2 = &Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	*address2 = Address{"Jakarta", "DKI Jakarta", "Indonesia"}
+	fmt.Println(address1)
+	fmt.Println(address2)
+}
